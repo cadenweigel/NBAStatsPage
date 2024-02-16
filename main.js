9b90ec94018005395b137856bae1bd87b6385afa
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2024-02-16 22:39:55 +0000

    finished Player object constructor

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function manageData(data) {
     //Data is usable here
     //console.log(data);
-    console.log(data[0]);
+    console.log(data[1]);
 }
 
 function parseData(url, callBack) {
@@ -16,11 +16,43 @@ function parseData(url, callBack) {
     });
 }
 
-parseData("data/player_stats_2324.csv", manageData);
-
 //convert object from csv to new object array
 
-function Player(name){
+function Player(name, age, team, gamesPlayed, gamesStarted, minutesPerGame,
+                fieldGoals, fieldGoalsAttempted, fieldGoalPercent,
+                threePointers, threePointersAttempted, threePointPercent,
+                twoPointers, twoPointersAttempted, twoPointPercent, effectiveFieldGoalPercent,
+                freeThrows, freeThrowsAttempted, freeThrowPercent,
+                offensiveReboundsPerGame, defensiveReboundsPerGame, reboundsPerGame,
+                assistsPerGame, stealsPerGame, blocksPerGame, turnoversPerGame, foulsPerGame, pointsPerGame){
     this.name = name;
+    this.age = age;
+    this.team = team;
+    this.gamesPlayed = gamesPlayed;
+    this.gamesStarted = gamesStarted;
+    this.minutesPerGame = minutesPerGame;
+    this.fieldGoals = fieldGoals;
+    this.fieldGoalsAttempted = fieldGoalsAttempted;
+    this.fieldGoalPercent = fieldGoalPercent;
+    this.threePointers = threePointers;
+    this.threePointersAttempted = threePointersAttempted;
+    this.threePointPercent = threePointPercent;
+    this.twoPointers = twoPointers;
+    this.twoPointersAttempted = twoPointersAttempted;
+    this.twoPointPercent = twoPointPercent;
+    this.effectiveFieldGoalPercent = effectiveFieldGoalPercent;
+    this.freeThrows = freeThrows;
+    this.freeThrowsAttempted = freeThrowsAttempted;
+    this.freeThrowPercent = freeThrowPercent;
+    this.offensiveReboundsPerGame = offensiveReboundsPerGame;
+    this.defensiveReboundsPerGame = defensiveReboundsPerGame;
+    this.reboundsPerGame = reboundsPerGame;
+    this.assistsPerGame = assistsPerGame;
+    this.stealsPerGame = stealsPerGame;
+    this.blocksPerGame = blocksPerGame;
+    this.turnoversPerGame = turnoversPerGame;
+    this.foulsPerGame = foulsPerGame;
+    this.pointsPerGame = pointsPerGame;
 }
 
+parseData("data/player_stats_2324.csv", manageData);
