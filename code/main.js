5bc2fcb740a1cfd8df2d8c4759a30283bb8cef50
cadenweigel <157074200+cadenweigel@users.noreply.github.com>
2024-02-16 05:43:42 +0000

    some js

--- code/main.js
+++ code/main.js
@@ -1,3 +1,20 @@
-//import module for csv
-var csv = require('jquery-csv');
+//set up csv
+function parseCSV(){
+    Papa.parse(/data/player_stats_2324.csv, {
+        complete: function(results) {
+            console.log("Finished:", results.data);
+        }
+    });
+}
+
+parseCSV();
+
+//convert csv to object
+
+
+//convert object from csv to new object array
+
+function Player(name){
+    this.name = name;
+}
 
