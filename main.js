1663b65db3695d64c584adb9ea38973ea5e28d2a
cadenweigel <cadenweigel4@gmail.com>
2024-02-22 16:30:42 -0800

    makeTable/fillRow functions, some css

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function manageData(data) {
     //create array of player objects
     let players = [];
     for (i = 1; i < data.length; i++){
-        //console.log(data[i][1]);
+        console.log(data[i][1]);
         let p = new Player(data[i][1],data[i][2],data[i][3],data[i][4],data[i][5],data[i][6],
                            data[i][7],data[i][8],data[i][9],data[i][10],data[i][11],data[i][12],
                            data[i][13],data[i][14],data[i][15],data[i][16],data[i][17],data[i][18],
@@ -48,14 +48,85 @@ function makeTable(header, players){
 
     //Create header row for table 
     var headerRow = table.insertRow(0);
-    for(let i = 0; i < header.length, i++){
+    for(let i = 0; i < header.length; i++){
         let cell = headerRow.insertCell(i);
         cell.innerHTML = header[i];
     }
 
+    for(let j = 0; j < players.length; j++){
+        fillRow(j+1, players[j], table);
+    }
+
 }
 
-function Player(name, age, team, gamesPlayed, gamesStarted, minutesPerGame,
+function fillRow(rowNum, player, table, row){
+    var row = table.insertRow(rowNum);
+
+    //The following code is very repetitive, since I assigned each data point to its own 
+    //attribute in player rather than just having an array and keeping track
+    //of where each stat was in the array
+    let col0 = row.insertCell(0);
+    col0.innerHTML = player.name;
+    let col1 = row.insertCell(1);
+    col1.innerHTML = player.position;
+    let col2 = row.insertCell(2);
+    col2.innerHTML = player.age;
+    let col3 = row.insertCell(3);
+    col3.innerHTML = player.team;
+    let col4 = row.insertCell(4);
+    col4.innerHTML = player.gamesPlayed;
+    let col5 = row.insertCell(5);
+    col5.innerHTML = player.gamesStarted;
+    let col6 = row.insertCell(6);
+    col6.innerHTML = player.minutesPerGame;
+    let col7 = row.insertCell(7);
+    col7.innerHTML = player.fieldGoals;
+    let col8 = row.insertCell(8);
+    col8.innerHTML = player.fieldGoalsAttempted;
+    let col9 = row.insertCell(9);
+    col9.innerHTML = player.fieldGoalPercent;
+    let col10 = row.insertCell(10);
+    col10.innerHTML = player.threePointers;
+    let col11 = row.insertCell(11);
+    col11.innerHTML = player.threePointersAttempted;
+    let col12 = row.insertCell(12);
+    col12.innerHTML = player.threePointPercent;
+    let col13 = row.insertCell(13);
+    col13.innerHTML = player.twoPointers;
+    let col14 = row.insertCell(14);
+    col14.innerHTML = player.twoPointersAttempted;
+    let col15 = row.insertCell(15);
+    col15.innerHTML = player.twoPointPercent;
+    let col16 = row.insertCell(16);
+    col16.innerHTML = player.effectiveFieldGoalPercent;
+    let col17 = row.insertCell(17);
+    col17.innerHTML = player.freeThrows;
+    let col18 = row.insertCell(18);
+    col18.innerHTML = player.freeThrowsAttempted;
+    let col19 = row.insertCell(19);
+    col19.innerHTML = player.freeThrowPercent;
+    let col20 = row.insertCell(20);
+    col20.innerHTML = player.offensiveReboundsPerGame;
+    let col21 = row.insertCell(21);
+    col21.innerHTML = player.defensiveReboundsPerGame;
+    let col22 = row.insertCell(22);
+    col22.innerHTML = player.reboundsPerGame;
+    let col23 = row.insertCell(23);
+    col23.innerHTML = player.assistsPerGame;
+    let col24 = row.insertCell(24);
+    col24.innerHTML = player.stealsPerGame;
+    let col25 = row.insertCell(25);
+    col25.innerHTML = player.blocksPerGame;
+    let col26 = row.insertCell(26);
+    col26.innerHTML = player.turnoversPerGame;
+    let col27 = row.insertCell(27);
+    col27.innerHTML = player.foulsPerGame;
+    let col28 = row.insertCell(28);
+    col28.innerHTML = player.pointsPerGame;
+
+}
+
+function Player(name, position, age, team, gamesPlayed, gamesStarted, minutesPerGame,
                 fieldGoals, fieldGoalsAttempted, fieldGoalPercent,
                 threePointers, threePointersAttempted, threePointPercent,
                 twoPointers, twoPointersAttempted, twoPointPercent, effectiveFieldGoalPercent,
@@ -63,6 +134,7 @@ function Player(name, age, team, gamesPlayed, gamesStarted, minutesPerGame,
                 offensiveReboundsPerGame, defensiveReboundsPerGame, reboundsPerGame,
                 assistsPerGame, stealsPerGame, blocksPerGame, turnoversPerGame, foulsPerGame, pointsPerGame){
     this.name = name;
+    this.position = position;
     this.age = age;
     this.team = team;
     this.gamesPlayed = gamesPlayed;
@@ -93,4 +165,3 @@ function Player(name, age, team, gamesPlayed, gamesStarted, minutesPerGame,
 }
 
 parseData("data/player_stats_2324.csv", manageData);
-//console.log(data);
